d5e23bf5c3f28ba566c91cda288caa6273efe0a5
William <mailliwrd@gmail.com>
2023-12-20 20:12:33 -0700

    6 python syntax practices

--- 06_single_letter_count/single_letter_count.py
+++ 06_single_letter_count/single_letter_count.py
@@ -1,4 +1,12 @@
 def single_letter_count(word, letter):
+    count = 0
+    word_list = [letters for letters in word]
+    print(word_list)
+    for ltrs in word_list:
+        if ltrs == letter:
+            count += 1
+    return count
+    
     """How many times does letter appear in word (case-insensitively)?
     
         >>> single_letter_count('Hello World', 'h')
