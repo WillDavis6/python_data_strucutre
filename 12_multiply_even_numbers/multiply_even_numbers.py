a4840164f94647802d7a5b0551b7af5654865467
William <mailliwrd@gmail.com>
2023-12-23 10:44:41 -0700

    commtits

--- 12_multiply_even_numbers/multiply_even_numbers.py
+++ 12_multiply_even_numbers/multiply_even_numbers.py
@@ -1,4 +1,18 @@
 def multiply_even_numbers(nums):
+    evens = list()
+    total = 0
+    for num in nums:
+        if num % 2 == 0:
+            evens.append(num)
+    for even in evens:
+        if total == 0:
+            total += even
+            print(total)
+        elif total !=0:               
+            total = total * even
+            print(total)
+    return total
+        
     """Multiply the even numbers.
     
         >>> multiply_even_numbers([2, 3, 4, 5, 6])
