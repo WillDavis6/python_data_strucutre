18aeea039f43f33ae806629a532e965816786b07
William <mailliwrd@gmail.com>
2023-12-26 10:32:02 -0600

    commiting ex 17

--- 17_mode/mode.py
+++ 17_mode/mode.py
@@ -1,4 +1,16 @@
 def mode(nums):
+    nums_set = set(nums)
+    most_common_num = 0
+    num_of_nums = 0
+    for num in nums_set:
+        count = nums.count(num)
+        if count > num_of_nums:
+            num_of_nums = count
+            most_common_num = num
+    return most_common_num
+
+    
+
     """Return most-common number in list.
 
     For this function, there will always be a single-most-common value;
