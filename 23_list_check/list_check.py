b3428a416972d64853b7484f4e1593fb6d441dac
William <mailliwrd@gmail.com>
2023-12-27 10:19:51 -0600

    commiting 23

--- 23_list_check/list_check.py
+++ 23_list_check/list_check.py
@@ -1,4 +1,9 @@
 def list_check(lst):
+    for item in lst:
+        if isinstance(item, list) != True:
+            return False
+    return True
+
     """Are all items in lst a list?
 
         >>> list_check([[1], [2, 3]])
